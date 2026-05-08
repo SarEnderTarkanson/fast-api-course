0d2d5267c1ae676a9b3538b998a633289cc99f41
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2026-05-09 01:57:37 +0200

    2 - 15 comments

---
+++ 01-python-project/lecture-one/Comments.py
@@ -0,0 +1,26 @@
+#Going to print Hello World!
+#print("Hello World!")
+print("Hi Alpy")
+
+# This is
+# going over
+# multiple lines
+
+"""
+This is a multiple line comment
+2
+3
+4
+"""
+
+'''
+This
+is
+another
+way
+to
+write
+multiple
+line
+comments
+'''

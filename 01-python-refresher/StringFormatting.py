3d8102e67810e1efe1970002af304d1f06998180
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2026-05-09 02:21:22 +0200

    2 - 18 string formatting in python

---
+++ 01-python-refresher/StringFormatting.py
@@ -0,0 +1,9 @@
+first_name="Alpy"
+
+print(f"Hi {first_name}")
+
+sentence="Hi {} {}"
+last_name="Tarkanson"
+
+print(sentence.format(first_name, last_name))
+print(f"Hi {first_name} {last_name}. I hope you're learning.")

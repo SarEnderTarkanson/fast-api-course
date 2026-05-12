e86d8d52d96c074390eac8ccabeedf95744840d1
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2026-05-12 02:16:22 +0200

    2-26 booleans and operators

---
+++ 02-python-refresher/BooleansAndOperators.py
@@ -0,0 +1,22 @@
+like_coffee=True
+like_tea=False
+
+favourite_food="pizza"
+favourite_number=42
+
+print(type(like_coffee))
+print(type(like_tea))
+print(type(favourite_food))
+print(type(favourite_number))
+
+print(1==2)
+print(1!=2)
+print(1>2)
+print(1<2)
+print(1>=2)
+print(1<=2)
+
+print(1>3 and 5<7)
+print(1>3 or 5<7)
+print(not 1>3)
+print(not(1==1))

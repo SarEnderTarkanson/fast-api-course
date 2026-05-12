480d17ce7764315d53216eaab58ee7a003bd4be3
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2026-05-12 02:00:08 +0200

    2-23 sets and tuples

---
+++ 02-python-refresher/SetsAndTuples.py
@@ -0,0 +1,24 @@
+my_set={1,2,3,4,5,1,2}
+
+print(my_set)
+
+print(len(my_set))
+
+for x in my_set:
+    print(x)
+
+my_set.discard(3)
+print(my_set)
+
+my_set.add(6)
+print(my_set)
+
+my_set.update([7,8])
+print(my_set)
+
+my_tuple=(1,2,3,4,5)
+print(my_tuple)
+print(len(my_tuple))
+print(my_tuple[0])
+
+my_tuple[0] = 100

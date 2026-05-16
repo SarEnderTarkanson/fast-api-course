a85f2f8d8a4ed3a3ee250689692ae16d1d79ee2e
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2026-05-16 02:12:53 +0200

    2-31 loops assignment

---
+++ 02-python-refresher/LoopsAssignment.py
@@ -0,0 +1,11 @@
+my_list = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+
+i = 0
+
+while i<3:
+    i += 1
+    for j in my_list:
+        if j == "Monday":
+            print("---------")
+            continue
+        print(j)

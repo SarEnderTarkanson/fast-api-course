048ecb5a0d84bcba6a1039f8404ac00abbcdfc91
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2026-05-20 02:52:18 +0200

    2-39 imports in python

---
+++ 02-python-refresher/imports/homework_grades.py
@@ -0,0 +1,10 @@
+from grade_average_service import calculate_homework as grade_service
+
+homework_assignment_grade = {
+    'homework_1': 85,
+    'homework_2': 100,
+    'homework_3': 81
+}
+
+
+grade_service(homework_assignments_arg=homework_assignment_grade)
---
+++ 02-python-refresher/imports/standardLib.py
@@ -0,0 +1,11 @@
+import random
+import math
+
+types_of_drinks = ["soda", "coffee", "water", "tea"]
+
+print(random.choice(types_of_drinks))
+
+print(random.randint(1,10))
+
+square_root = math.sqrt(64)
+print(int(square_root))

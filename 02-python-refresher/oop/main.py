525499336345205e641483a89fa545c14de156ce
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2026-05-25 02:44:22 +0200

    2-46 constructors in python

--- 02-python-refresher/oop/main.py
+++ 02-python-refresher/oop/main.py
@@ -1,6 +1,6 @@
 from Enemy import *
 
-zombie = Enemy()
-zombie.type_of_enemy = "Zombie"
+zombie = Enemy(type_of_enemy="Zombie", health_points=10, attack_damage=1)
+zombie.type_of_enemy = "Ogre"
 
 print(zombie.attack())

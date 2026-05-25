4650948b13dd2cc863d9c027a22694f69e08291f
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2026-05-25 03:19:45 +0200

    2-48 encapsulation in python

--- 02-python-refresher/oop/Enemy.py
+++ 02-python-refresher/oop/Enemy.py
@@ -1,16 +1,19 @@
 class Enemy:
 
     def __init__(self, type_of_enemy, health_points, attack_damage):
-        self.type_of_enemy = type_of_enemy
+        self.__type_of_enemy = type_of_enemy
         self.health_points = health_points
         self.attack_damage = attack_damage
 
     def talk(self):
-        print(f"I am a {self.type_of_enemy}. "
+        print(f"I am a {self.__type_of_enemy}. "
               f"Be prepared to fight")
 
     def walk_toward(self):
-        print(f"{self.type_of_enemy} moves closer to you")
+        print(f"{self.__type_of_enemy} moves closer to you")
 
     def attack(self):
-        print(f"{self.type_of_enemy} attacks for {self.attack_damage} damage.")
+        print(f"{self.__type_of_enemy} attacks for {self.attack_damage} damage.")
+
+    def get_type_of_enemy(self):
+        return self.__type_of_enemy
--- 02-python-refresher/oop/main.py
+++ 02-python-refresher/oop/main.py
@@ -1,6 +1,6 @@
 from Enemy import *
 
 zombie = Enemy(type_of_enemy="Zombie", health_points=10, attack_damage=1)
-zombie.type_of_enemy = "Ogre"
 
-print(zombie.attack())
+
+print(zombie.get_type_of_enemy())

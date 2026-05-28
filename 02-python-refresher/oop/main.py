e0b4374541fe4af8b715f4cf75609f15848e1284
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2026-05-29 00:56:26 +0200

    2-55 polymorphism in python

--- 02-python-refresher/oop/main.py
+++ 02-python-refresher/oop/main.py
@@ -2,12 +2,12 @@ from Enemy import *
 from Zombie import *
 from Ogre import *
 
+def battle(e: Enemy):
+    e.talk()
+    e.attack()
+
 zombie = Zombie(health_points=10, attack_damage=1)
 ogre = Ogre(20,3)
 
-
-zombie.talk()
-#zombie.spread_disease()
-
-ogre.talk()
-#ogre.attack()
+battle(zombie)
+battle(ogre)

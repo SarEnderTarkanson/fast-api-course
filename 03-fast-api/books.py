6358c2957757176dea3bba1cca5a3de29ae420f9
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2026-06-07 03:13:48 +0200

    5-69 fastapi project - enhance get request

--- 03-fast-api/books.py
+++ 03-fast-api/books.py
@@ -2,6 +2,16 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/api-endpoint")
-def first_api():
-    return {"message": "Hello Alpy!"}
+BOOKS = [
+    {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
+    {'title': 'Title Two', 'author': 'Author Two', 'category': 'sci-fi'},
+    {'title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
+    {'title': 'Title Four', 'author': 'Author Four', 'category': 'math'},
+    {'title': 'Title Five', 'author': 'Author Five', 'category': 'arts'},
+    {'title': 'Title Six', 'author': 'Author Two', 'category': 'philosophy'},
+]
+
+
+@app.get("/books")
+def read_all_books():
+    return BOOKS

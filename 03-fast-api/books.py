b82150fa49814b706638e0725809b366bc38e7f5
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2026-06-27 23:34:30 +0200

    5-79 delete request

--- 03-fast-api/books.py
+++ 03-fast-api/books.py
@@ -19,14 +19,14 @@ def read_all_books():
 @app.get("/books/{book_title}")
 async def read_book(book_title: str):
     for book in BOOKS:
-        if book.get('title').casefold() == book_title.casefold():
+        if (book.get('title') or '').casefold() == book_title.casefold():
             return book
 
 @app.get("/books/")
 async def read_category_by_query(category: str):
     books_to_return = []
     for book in BOOKS:
-        if book.get('category').casefold() == category.casefold():
+        if (book.get('category') or '').casefold() == category.casefold():
             books_to_return.append(book)
     return books_to_return
 
@@ -34,8 +34,8 @@ async def read_category_by_query(category: str):
 async def read_author_category_by_query(book_author:str, category:str):
     books_to_return = []
     for book in BOOKS:
-        if book.get('author').casefold() == book_author.casefold() and \
-            book.get('category').casefold() == category.casefold():
+        if (book.get('author') or '').casefold() == book_author.casefold() and \
+            (book.get('category') or '').casefold() == category.casefold():
             books_to_return.append(book)
 
     return books_to_return
@@ -47,5 +47,12 @@ async def create_book(new_book=Body()):
 @app.put("/books/update_book")
 async def update_book(updated_book=Body()):
     for i in range(len(BOOKS)):
-        if BOOKS[i].get('title').casefold() == updated_book.get('title').casefold():
+        if (BOOKS[i].get('title') or '').casefold() == (updated_book.get('title') or '').casefold():
             BOOKS[i] = updated_book
+
+@app.delete("/books/delete_book/{book_title}")
+async def delete_book(book_title: str):
+    for i in range(len(BOOKS)):
+        if (BOOKS[i].get('title') or '').casefold() == book_title.casefold():
+            BOOKS.pop(i)
+            break
